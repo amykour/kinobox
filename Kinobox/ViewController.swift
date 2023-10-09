4f36079f962486469425b523bc79db49cc4ec342
Alena <amy.kour@yandex.ru>
2023-10-09 23:54:49 +0300

    Initial Commit

---
+++ Kinobox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kinobox
+//
+//  Created by Алена on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
